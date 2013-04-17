2599ad030d10d6e81a8f7ca809ceaa87b5cb4faf
Thomas <darkdream.wood@gmail.com>
2013-04-16 17:43:18 -0700

    checkpoint

--- link_main.c
+++ link_main.c
@@ -1,4 +1,5 @@
 #include "tuple.h"
+#include <stdio.h>
 
 typedef void *Space;
 
@@ -14,21 +15,31 @@ bool node_Allocate(const Space space,
            atom,
            size,
            target);
+
+    target.reference[0] = 32;
+
     return true;
 }
 
-void enki_test() {
-    asm("pusha\n\t"
-        "movl $1,-8(%esp)\n\t"
-        "movl $2,-12(%esp)\n\t"
-        "lea -12(%esp),%eax\n\t"
+void enki_test(void* one, void* two) {
+    void* holding = 0;
+
+    asm("movl %1,-8(%%esp)\n\t"
+        "movl %2,-12(%%esp)\n\t"
+        "lea -12(%%esp),%%eax\n\t"
         "call alloc_gc\n\t"
-        "popa");
+        "movl %%eax,%0"
+        : "=r" (holding)
+        : "r" (one), "r" (two)
+        : "%eax", "%ebx", "%esi", "edi"
+        );
+
+    printf("results %p\n", holding);
 }
 
 int main(int argc, char** argv) {
     printf("_zero_space %p\n", _zero_space);
-    enki_test();
+    enki_test(1,2);
     printf("done\n");
     return 0;
 }
