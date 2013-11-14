c0eb5a3854efcbe25216d7844de69d1dbf8e09e1
Thomas <darkdream.wood@gmail.com>
2013-11-13 19:36:00 -0800

    checkpoint

---
+++ buildins/conditionals.c
@@ -0,0 +1,141 @@
+/***************************
+ **
+ ** Project: *current project*
+ **
+ ** Routine List:
+ **    <routine-list-end>
+ **/
+#define _GNU_SOURCE
+#define debug_THIS
+#include "all_types.inc"
+#include "treadmill.h"
+#include "debug.h"
+#include "text_buffer.h"
+#include "id_set.h"
+
+/* */
+#include <error.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+/* */
+
+#define SUBR(NAME) void opr_##NAME(Node args, Node env, Target result)
+#define APPLY(NAME,ARGS,ENV,RESULT) opr_##NAME(ARGS,ENV,RESULT)
+
+/* */
+/* */
+
+extern SUBR(if)
+{ //Fixed
+    GC_Begin(2);
+
+    Node tst, val, t_expr, e_body;
+
+    GC_Protect(val);
+
+    list_GetItem(args.pair, 0, &tst);
+    list_GetItem(args.pair, 1, &t_expr);
+    list_GetTail(args.pair, 1, &e_body);
+
+    eval(tst, env, &val);
+
+    if (isNil(val)) {
+        eval_begin(e_body, env, result);
+    } else {
+        eval(t_expr, env, result);
+    }
+    GC_End();
+}
+
+extern SUBR(unless)
+{ //Fixed
+    GC_Begin(2);
+
+    Node tst, val, t_expr, e_body;
+
+    GC_Protect(val);
+
+    list_GetItem(args.pair, 0, &tst);
+    list_GetItem(args.pair, 1, &t_expr);
+    list_GetTail(args.pair, 1, &e_body);
+
+    eval(tst, env, &val);
+
+    if (isNil(val)) {
+        eval(t_expr, env, result);
+    } else {
+        eval_begin(e_body, env, result);
+    }
+    GC_End();
+}
+
+extern SUBR(and)
+{ //Fixed
+    GC_Begin(2);
+    Node body, expr, ans;
+
+    GC_Protect(ans);
+
+    body = args;
+    expr = NIL;
+    ans  = true_v;
+
+    for (; isPair(body) ;) {
+        pair_GetCar(body.pair, &expr);
+        pair_GetCdr(body.pair, &body);
+        eval(expr, env, &ans);
+        if (isNil(ans)) break;
+    }
+
+    ASSIGN(result, ans);
+    GC_End();
+}
+
+extern SUBR(or)
+{ //Fixed
+    GC_Begin(2);
+    Node body, expr, ans;
+
+    GC_Protect(ans);
+
+    body = args;
+    expr = NIL;
+    ans  = NIL;
+
+    for (; isPair(body) ;) {
+        pair_GetCar(body.pair, &expr);
+        pair_GetCdr(body.pair, &body);
+        eval(expr, env, &ans);
+        if (!isNil(ans)) break;
+    }
+
+    ASSIGN(result, ans);
+    GC_End();
+}
+
+extern SUBR(while)
+{ //Fixed
+    Node tst  = NIL;
+    Node body = NIL;
+    Node val  = NIL;
+
+    pair_GetCar(args.pair, &tst);
+    pair_GetCdr(args.pair, &body);
+
+    for (;;) {
+        eval(tst, env, &val);
+
+        if (isNil(val)) break;
+
+        eval_begin(body, env, result);
+    }
+}
